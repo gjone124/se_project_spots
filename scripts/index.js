dc7921b247c1b5ba3a4dc90a80408f4c170a4f3b
gjone124 <gjone124@gmail.com>
2024-08-12 21:57:28 -0400

    event listeners for edit profile button added

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,18 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const editModal = document.querySelector("#edit-modal");
+const editModalCloseButton = document.querySelector(".modal__close-button");
+
+function openModal() {
+  editModal.classList.add("modal__opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+editModalCloseButton.addEventListener("click", closeModal);
