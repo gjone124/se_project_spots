dd0ae2b44a26683539e39b6d129b40cf0e23a1ad
gjone124 <gjone124@gmail.com>
2024-09-11 06:34:21 -0400

    validation implemented

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,10 @@ const addCardModalButton = document.querySelector(".profile__add-button");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
-// Edit Profile Form Elements
+// Modal Form Elements
+const modals = document.querySelectorAll(".modal");
+
+// Edit Profile Modal Form Elements
 const editProfileModal = document.querySelector("#edit-modal");
 const editProfileFormElement = editProfileModal.querySelector(".modal__form");
 const editProfileModalCloseButton = editProfileModal.querySelector(
@@ -54,20 +57,23 @@ const editProfileModalDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
 
-// Add Card Form Elements
+// Add Card (aka New Post) Modal Form Elements
 const addCardModal = document.querySelector("#add-card-modal");
 const addCardFormElement = addCardModal.querySelector(".modal__form");
+const addCardModalSubmitButton = addCardModal.querySelector(
+  ".modal__submit-button"
+);
 const addCardModalCloseButton = addCardModal.querySelector(
   ".modal__close-button"
 );
-const addCardModalNameInput = addCardModal.querySelector(
-  "#add-card-name-input"
+const addCardModalCaptionInput = addCardModal.querySelector(
+  "#add-card-caption-input"
 );
 const addCardModalLinkInput = addCardModal.querySelector(
   "#add-card-link-input"
 );
 
-// Preview Modal Elements
+// Preview Modal Form Elements
 const previewModal = document.querySelector("#preview-modal");
 const previewModalCaptionElement =
   previewModal.querySelector(".modal__caption");
@@ -80,50 +86,21 @@ const previewModalCloseButton = previewModal.querySelector(
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
 
-function openModal(modal) {
-  modal.classList.add("modal_is-opened");
-}
-
-function closeModal(modal) {
-  modal.classList.remove("modal_is-opened");
-}
-
-function handleEditProfileFormSubmit(event) {
-  event.preventDefault();
-  profileName.textContent = editProfileModalNameInput.value;
-  profileDescription.textContent = editProfileModalDescriptionInput.value;
-  closeModal(editProfileModal);
-}
-
-function handleAddCardFormSubmit(event) {
-  event.preventDefault();
-  const inputValues = {
-    name: addCardModalNameInput.value,
-    link: addCardModalLinkInput.value,
-  };
-
-  const addCardElement = getCardElement(inputValues);
-  cardsList.prepend(addCardElement);
-  closeModal(addCardModal);
-  event.target.reset();
-}
-
+// Function To Create A Card
 function getCardElement(data) {
   const cardElement = cardTemplate.content
     .querySelector(".card")
     .cloneNode(true);
 
-  const cardNameElement = cardElement.querySelector(".card__text");
+  const cardCaptionElement = cardElement.querySelector(".card__text");
   const cardImageElement = cardElement.querySelector(".card__image");
-  const cardLikeButton = cardElement.querySelector(".card__like-button");
-  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
-  //select the delete button
-  //set the listener on the delete button
-  //remove card from DOM using handler
 
-  cardNameElement.textContent = data.name;
+  cardCaptionElement.textContent = data.name;
   cardImageElement.src = data.link;
   cardImageElement.alt = data.name;
+
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
   // Like Button Event Listener
   cardLikeButton.addEventListener("click", () => {
@@ -146,17 +123,78 @@ function getCardElement(data) {
   return cardElement;
 }
 
+// Function To Open Modal
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+  document.addEventListener("keydown", handleEscapeKey);
+}
+
+// Function To Close Modal
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+  document.removeEventListener("keydown", handleEscapeKey);
+}
+
+// Function To Close Modal By Clicking Overlay
+modals.forEach((modal) => {
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal(modal);
+    }
+  });
+});
+
+// Function To Close Modal By Pressing Escape Key ("Esc")
+function handleEscapeKey(event) {
+  if (event.key === "Escape") {
+    const openModal = document.querySelector(".modal_is-opened");
+    if (openModal) {
+      closeModal(openModal);
+    }
+  }
+}
+
+// Function To Handle Pressing Submit Button For Edit Profile Modal
+function handleEditProfileFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = editProfileModalNameInput.value;
+  profileDescription.textContent = editProfileModalDescriptionInput.value;
+  closeModal(editProfileModal);
+}
+
+// Function To Handle Pressing Submit Button For Add Card (New Post) Modal
+function handleAddCardFormSubmit(event) {
+  event.preventDefault();
+  const inputValues = {
+    name: addCardModalCaptionInput.value,
+    link: addCardModalLinkInput.value,
+  };
+
+  const addCardElement = getCardElement(inputValues);
+  cardsList.prepend(addCardElement);
+  event.target.reset();
+  disableButton(addCardModalSubmitButton, settings);
+  closeModal(addCardModal);
+}
+
 // Edit Profile Modal Event Listeners
 editProfileModalButton.addEventListener("click", () => {
   editProfileModalNameInput.value = profileName.textContent.trim();
   editProfileModalDescriptionInput.value =
     profileDescription.textContent.trim();
+  resetValidation(
+    editProfileModal,
+    [editProfileModalNameInput, editProfileModalDescriptionInput],
+    settings
+  );
   openModal(editProfileModal);
 });
 
 editProfileModalCloseButton.addEventListener("click", () => {
   closeModal(editProfileModal);
 });
+
+editProfileFormElement.addEventListener("submit", handleEditProfileFormSubmit);
 
 // Add Card Modal Event Listeners
 addCardModalButton.addEventListener("click", () => {
@@ -167,14 +205,14 @@ addCardModalCloseButton.addEventListener("click", () => {
   closeModal(addCardModal);
 });
 
+addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+
 // Preview Modal Event Listener
 previewModalCloseButton.addEventListener("click", () => {
   closeModal(previewModal);
 });
 
-editProfileFormElement.addEventListener("submit", handleEditProfileFormSubmit);
-addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
-
+// Function To Display All Of The Cards
 initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
   cardsList.append(cardElement);
---
+++ scripts/validation.js
@@ -0,0 +1,90 @@
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__submit-button",
+  inactiveButtonClass: "modal__submit-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error",
+};
+
+const showInputError = (formElement, inputElement, errorMessage, config) => {
+  const errorMessageID = inputElement.id + "-error";
+  const errorMessageElement = formElement.querySelector("#" + errorMessageID);
+  errorMessageElement.textContent = errorMessage;
+  inputElement.classList.add(config.inputErrorClass);
+};
+
+const hideInputError = (formElement, inputElement, config) => {
+  const errorMessageID = inputElement.id + "-error";
+  const errorMessageElement = formElement.querySelector("#" + errorMessageID);
+  errorMessageElement.textContent = "";
+  inputElement.classList.remove(config.inputErrorClass);
+};
+
+const checkInputValidity = (formElement, inputElement, config) => {
+  if (!inputElement.validity.valid) {
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      config
+    );
+  } else {
+    hideInputError(formElement, inputElement, config);
+  }
+};
+
+const hasInvalidInput = (inputList, config) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, config) => {
+  if (hasInvalidInput(inputList, config)) {
+    disableButton(buttonElement, config);
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove(config.inactiveButtonClass);
+  }
+};
+
+const disableButton = (buttonElement, config) => {
+  buttonElement.disabled = true;
+  buttonElement.classList.add(config.inactiveButtonClass);
+};
+
+const resetValidation = (formElement, inputList, config) => {
+  inputList.forEach((input) => {
+    hideInputError(formElement, input, config);
+  });
+};
+
+const setEventListeners = (formElement, config) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(config.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement, config);
+
+  formElement.addEventListener("reset", () => {
+    disableButton(buttonElement, config);
+  });
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
+    });
+  });
+};
+
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, config);
+  });
+};
+
+enableValidation(settings);
